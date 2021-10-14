b4e29b1a6c273b55b006504c681a83ba01dfe37f
Matt Weichel <maweiche@gmail.com>
2021-10-14 07:30:39 -0700

    added badge content

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 
-# ls
+# a
+
+![badge](https://img.shields.io/badge/license-WTFPL-brightgreen)</br>
 
 ## Table of Contents
 1. [Description](#Description)
 2. [Installation](#Install)
-3. [Usage] (#Usage)
-4. [Contributing] (#Contribution)
-5. [Tests] (#Tests)
-6. [License] (#License)
-7. [GitHub] (#Github)
-8. [E-mail] (#E-mail)
+3. [Usage](#Usage)
+4. [Contributing](#Contribution)
+5. [Tests](#Tests)
+6. [License](#License)
+7. [GitHub](#Github)
+8. [E-mail](#E-mail)
 
 ## Description
 a
@@ -27,7 +29,7 @@ a
 a
 
 ## License
-GNU
+WTFPL
 
 ## Github
 a
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -2,15 +2,17 @@ function generateMarkdown(data) {
 return `
 # ${data.title}
 
+![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)</br>
+
 ## Table of Contents
 1. [Description](#Description)
 2. [Installation](#Install)
-3. [Usage] (#Usage)
-4. [Contributing] (#Contribution)
-5. [Tests] (#Tests)
-6. [License] (#License)
-7. [GitHub] (#Github)
-8. [E-mail] (#E-mail)
+3. [Usage](#Usage)
+4. [Contributing](#Contribution)
+5. [Tests](#Tests)
+6. [License](#License)
+7. [GitHub](#Github)
+8. [E-mail](#E-mail)
 
 ## Description
 ${data.description}
